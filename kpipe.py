694a89a170936fb69ee69cec28e4b649ec944dff
Kit <kittsville@googlemail.com>
2015-04-11 16:33:49 +0100

    Initial Commit
    Added script name/author/license/etc. and imported required modules

---
+++ kpipe.py
@@ -0,0 +1,12 @@
+'''
+Name:			KitPipe
+Description:	A crappy little assets minified created so I could minify my WP-Librarian assets a bit assets
+Author:			Kittsville
+Contact:		kittsville [at] gmail.com
+Requires:		cssmin, slimit, Python 3.4.3
+License:		GPL2
+'''
+
+import cssmin from cssmin # For CSS
+from slimit import minify # For JS
+
